2454da62da3e6a8f38d63a4d024187d438d07f45
Dogth <dogth@kitteth.com>
2024-09-04 23:11:06 +0300

    fix(cmp.nix): fix keymap collisions

--- packages/puppydog/plugins/lsp/cmp.nix
+++ packages/puppydog/plugins/lsp/cmp.nix
@@ -26,9 +26,9 @@
 
         mapping = {
           "<S-Space>" = "cmp.mapping.complete()";
-          "<S-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<S-f>" = "cmp.mapping.scroll_docs(4)";
-          "<S-e>" = "cmp.mapping.close()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-e>" = "cmp.mapping.close()";
           "<CR>" = "cmp.mapping.confirm({select = true})";
           "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i','s'})";
           "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i','s'})";
