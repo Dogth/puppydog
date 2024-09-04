6e47f43513ff41d5f80fd2fcb1f539eff1d77c06
Dogth <dogth@kitteth.com>
2024-09-04 22:47:57 +0300

    feat(hardtime.nix): add Hardtime toggle

--- packages/puppydog/plugins/utils/hardtime.nix
+++ packages/puppydog/plugins/utils/hardtime.nix
@@ -3,4 +3,15 @@
     enable = true;
     restrictionMode = "hint";
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>uH";
+      action = "<cmd>Hardtime toggle<CMD>";
+      options = {
+        desc = "Toggle Hardtime";
+        silent = true;
+      };
+    }
+  ];
 }
