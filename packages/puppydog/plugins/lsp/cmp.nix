5a53030a5fa068a3933fe3c984fa8d83464abebe
Dogth <dogth@kitteth.com>
2024-09-23 22:07:11 +0300

    plugin: add priorities to cmp sources

--- packages/puppydog/plugins/lsp/cmp.nix
+++ packages/puppydog/plugins/lsp/cmp.nix
@@ -12,16 +12,47 @@
       settings = {
         experimental.ghost_text = true;
         sources = [
-          {name = "nvim_lsp";}
-          {name = "path";}
-          {name = "cmdline";}
-          {name = "codeium";}
-          {name = "treesitter";}
-          {name = "buffer";}
+          {
+            name = "nvim_lsp";
+            priority = 100;
+          }
+          {
+            name = "path";
+            priority = 100;
+          }
+          {
+            name = "nvim_lsp_signature_help";
+            priority = 100;
+          }
+          {
+            name = "nvim_lsp_document_symbol";
+            priority = 100;
+          }
+          {
+            name = "cmdline";
+            priority = 50;
+          }
+          {
+            name = "codeium";
+            priority = 30;
+          }
+          {
+            name = "treesitter";
+            priority = 85;
+          }
+          {
+            name = "buffer";
+            priority = 50;
+          }
         ];
 
         formatting = {
           fields = ["kind" "abbr" "menu"];
+        };
+
+        window = {
+          completion.__raw = ''cmp.config.window.bordered'';
+          documentation.__raw = ''cmp.config.window.bordered'';
         };
 
         mapping = {
